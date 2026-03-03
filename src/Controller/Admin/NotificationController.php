1577972cfb1023eb58a05d43f0bc162f6ae9734b
Sanjay Kumar <sanjay@doomshell.com>
2026-03-03 15:14:25 +0530

    notification functionlity done

--- src/Controller/Admin/NotificationController.php
+++ src/Controller/Admin/NotificationController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Controller\Admin;
+
 use App\Controller\AppController;
 use Cake\Core\Configure;
 use Cake\Network\Exception\ForbiddenException;
@@ -7,6 +9,7 @@ use Cake\Network\Exception\NotFoundException;
 use Cake\ORM\TableRegistry;
 use Cake\View\Exception\MissingTemplateException;
 use Cake\Datasource\ConnectionManager;
+
 require_once "Firebase.php";
 require_once "Push.php";
 
@@ -79,6 +82,7 @@ class NotificationController extends AppController
                 }
                 $pushNotification = $push->getPush();
                 $response = $firebase->send($tokens, $pushNotification);
+                // pr($response); die;
                 $this->Flash->success(__("Notifications has been saved."));
                 return $this->redirect(["action" => "index"]);
             }
@@ -86,26 +90,29 @@ class NotificationController extends AppController
 
             //For Admin Start
             $newpack = $this->Notifications->newEntity();
-            $savepack = $this->Notifications->patchEntity( $newpack,$this->request->data);
+            $savepack = $this->Notifications->patchEntity($newpack, $this->request->data);
             $results = $this->Notifications->save($savepack);
 
             if ($results) {
-                $firebase = new \Firebase();
-                $push = new \Push(
-                    $this->request->data["title"],
-                    $this->request->data["message"]
-                );
-                $user = $this->Users->find("all")->contain(['Devices'])->where(["Users.status" => 1, "Users.role_id" => 3])->order(['Users.id'=>'desc'])->toarray();
+                // $firebase = new \Firebase();
+                // $push = new \Push(
+                //     $this->request->data["title"],
+                //     $this->request->data["message"]
+                // );
+                $user = $this->Users->find("all")->contain(['Devices'])->where(["Users.status" => 1, "Users.role_id" => 3])->order(['Users.id' => 'desc'])->toarray();
 
                 foreach ($user as $value) {
-                    $udata["notification_counter"] = $value["notification_counter"] + 1;
-                    $usavepack = $this->Users->patchEntity($value, $udata);
-                    $uresults = $this->Users->save($usavepack);
-                    $tokens[] = trim($value['device']["token"]);
+                    //     $udata["notification_counter"] = $value["notification_counter"] + 1;
+                    //     $usavepack = $this->Users->patchEntity($value, $udata);
+                    //     $uresults = $this->Users->save($usavepack);
+                    //     $tokens[] = trim($value['device']["token"]);
+
+                    $this->sendNotification($value['id'], $results['title'], $results['message']);
+            
                 }
-              
-                $pushNotification = $push->getPush();
-                $response = $firebase->send($tokens, $pushNotification);
+
+                // $pushNotification = $push->getPush();
+                // $response = $firebase->send($tokens, $pushNotification);
                 // pr($response);die;
                 //For Admin End
                 $this->Flash->success(__("Notifications has been saved."));
@@ -151,6 +158,134 @@ class NotificationController extends AppController
             );
             return $this->redirect(["action" => "index"]);
         }
+    }
+
+    // This is for send notification by firebase on 26-09-2024(Rajesh kumar)
+    public function sendNotification($userId, $title = null, $message = null)
+    {
+            
+        $serviceAccountFile = WWW_ROOT . 'gaouribrand-78d71-330c9abf21e8.json';
+
+        $accessToken = $this->getGoogleAccessToken($serviceAccountFile);
+        // pr($accessToken); die;
+        $url = 'https://fcm.googleapis.com/v1/projects/gaouribrand-78d71/messages:send';
+
+        $this->loadModel('Users');
+        $this->loadModel('Notifications');
+        $user = $this->Users->find('all')->where(['id' => $userId])->first();
+        // $token = $user['token'];
+        $token = $user['token'];
+        // pr($token); die;
+
+        // save notifications
+        $user->notification_counter = $user->notification_counter + 1;
+        $this->Users->save($user);
+
+
+        $message = [
+            'message' => [
+                'token' => $token,
+                'notification' => [
+                    'title' => $title,
+                    'body' => $message,
+                ],
+                'data' => [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ],
+            ],
+        ];
+
+        $jsonMessage = json_encode($message);
+
+        $headers = [
+            'Authorization: Bearer ' . $accessToken,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonMessage);
+
+        $response = curl_exec($ch);
+
+        if ($response === FALSE) {
+            die('FCM Send Error: ' . curl_error($ch));
+        }
+
+        curl_close($ch);
+
+        echo $response;
+    }
+
+
+
+
+    private function getGoogleAccessToken($serviceAccountFile)
+    {
+        $tokenUri = 'https://oauth2.googleapis.com/token';
+        $serviceAccount = json_decode(file_get_contents($serviceAccountFile), true);
+
+        function base64url_encode($data)
+        {
+            return rtrim(strtr(base64_encode($data), '+/', '-_'), '=');
+        }
+
+        $jwtHeader = base64url_encode(json_encode([
+            'alg' => 'RS256',
+            'typ' => 'JWT',
+        ]));
+
+        $jwtClaimSet = base64url_encode(json_encode([
+            'iss'   => $serviceAccount['client_email'],
+            'scope' => 'https://www.googleapis.com/auth/cloud-platform',
+            'aud'   => $tokenUri,
+            'exp'   => time() + 3600,
+            'iat'   => time(),
+        ]));
+
+        openssl_sign(
+            $jwtHeader . '.' . $jwtClaimSet,
+            $signature,
+            $serviceAccount['private_key'],
+            OPENSSL_ALGO_SHA256
+        );
+
+        $jwt = $jwtHeader . '.' . $jwtClaimSet . '.' . base64url_encode($signature);
+
+        $postFields = [
+            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'assertion'  => $jwt,
+        ];
+
+        $ch = curl_init($tokenUri);
+        curl_setopt_array($ch, [
+            CURLOPT_POST           => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS     => http_build_query($postFields),
+            CURLOPT_HTTPHEADER     => ['Content-Type: application/x-www-form-urlencoded'],
+            CURLOPT_SSLVERSION     => CURL_SSLVERSION_TLSv1_2,
+        ]);
+
+        $result = curl_exec($ch);
+
+        if ($result === false) {
+            die('cURL Error: ' . curl_error($ch));
+        }
+
+        curl_close($ch);
+
+        $response = json_decode($result, true);
+        // pr($response); die;
+
+        if (!isset($response['access_token'])) {
+            die('OAuth Error: ' . $result);
+        }
+
+        return $response['access_token'];
     }
 
 }
